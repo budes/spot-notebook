8925e1ded523de5910c8aaac1416f8f248f305e2
Samuel Brandão <74151920+budes@users.noreply.github.com>
2021-04-08 20:40:57 -0300

    Update gera_formatado.py

--- cgi-bin/gera_formatado.py
+++ cgi-bin/gera_formatado.py
@@ -3,6 +3,26 @@
 import cgi
 import os
 
-args = cgi.FieldStorage()
+# O caminho é relativo ao path inicial '/'
 
-caminho = args['caminho']
+args = {'arquivo': 'Roteiro.txt', 'caminho': 'Arquivos/'}# cgi.FieldStorage()
+
+caminho = args['caminho'].value
+arq = args['arquivo'].value
+
+os.chdir('..')
+os.chdir(caminho)
+
+arquivo = open(arq, 'r')
+
+mostrar = arquivo.read()
+
+print("Content-type: text/html")
+
+print(mostrar)
+
+arquivo.close()
+
+
+
+
